bc02235f250fc53294a1c80f58cc7ec5636102b0
Kathleen Ly <lykathleen@outlook.com>
2022-03-28 15:22:00 +0100

    Refactored fizzbuzz challenge
    Co-authored-by: Samuel Raducan <samuelmbp@users.noreply.github.com>

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,22 @@
-const fizzBuzz = () => {
-  for (let i = 1; i <= 50; i++){
-    if (i % 3 === 0 && i % 5 === 0) console.log(`Fizzbuzz result for ${i} is FizzBuzz`);
-    else if (i % 5 === 0) console.log(`Fizzbuzz result for ${i} is Buzz`);
-    else if (i % 3 === 0) console.log(`Fizzbuzz result for ${i} is Fizz`);
-    else console.log(`Fizzbuzz result for ${i} is ${i}`);
-  }
-}
+// const fizzBuzz = () => {
+//   for (let i = 1; i <= 50; i++){
+//     if (i % 3 === 0 && i % 5 === 0) console.log(`Fizzbuzz result for ${i} is FizzBuzz`);
+//     else if (i % 5 === 0) console.log(`Fizzbuzz result for ${i} is Buzz`);
+//     else if (i % 3 === 0) console.log(`Fizzbuzz result for ${i} is Fizz`);
+//     else console.log(`Fizzbuzz result for ${i} is ${i}`);
+//   }
+// }
 
-fizzBuzz()
+// fizzBuzz()
+
+const fizzBuzz = (number) => {
+  
+    if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+    else if (number % 5 === 0) return 'Buzz';
+    else if (number % 3 === 0) return 'Fizz';
+    else return number;
+  }
+
+for (let i = 1; i <= 50; i++){
+  console.log(`Fizzbuzz result for ${i} is ${fizzBuzz(i)}`)
+};
